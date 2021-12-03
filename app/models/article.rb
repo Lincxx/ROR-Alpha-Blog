6c809ecd59dff9c2196e8bbb180368f26e4b7f8b
Jeremy Lincoln <jeremy.lincoln@bisonline.com>
2021-12-03 10:31:25 -0500

    Add one to many association between users and articel form backend

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+    belongs_to :user
     # valdations this will stop an record from being inserted
     validates :title, presence: true, length: {minimum: 6, mazimum: 100}
     validates :description, presence: true, length: {minimum: 10, mazimum: 300}
