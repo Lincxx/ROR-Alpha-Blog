2d479ebaf5a76e3b09184375637632b70c91889a
Lincxx <bossco28@gmail.com>
2021-12-06 07:06:33 -0500

    Move the helper_methods to the application_controller and declared them as helper_methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+    
+    helper_method :current_user, :logged_in? # this makes it available to our views
+
+    def current_user
+        #if session[:user_id] 
+        #   User.find(session[:user_id])
+        # this is an other way of writing it
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+        #return the current_user, if there is no current_user them find the user
+        #nice thing about this is we will not hit the db reapeatly to find the user
+        
+    end
+
+    def logged_in?
+        !!current_user
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,17 +7,4 @@ module ApplicationHelper
         image_tag(gravatar_url, alt: user.username, class: "rounded mt-3 shadow mx-auto d-block" )    
     end
 
-    def current_user
-        #if session[:user_id] 
-        #   User.find(session[:user_id])
-        # this is an other way of writing it
-        @current_user ||= User.find(session[:user_id]) if session[:user_id]
-        #return the current_user, if there is no current_user them find the user
-        #nice thing about this is we will not hit the db reapeatly to find the user
-        
-    end
-
-    def logged_in?
-        !!current_user
-    end
 end
