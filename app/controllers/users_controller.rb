169680e8e7c2d8db5aa2ffb8ffa2830a7c4d1b7e
Lincxx <bossco28@gmail.com>
2021-12-06 06:51:16 -0500

    Add some helper methods for current_user and logg_in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,20 @@
 class UsersController < ApplicationController
-
+    before_action :set_user, only: [:show, :edit, :update]
     def index
         @users = User.paginate(page: params[:page], per_page: 5)
     end 
 
     def show
-        @user = User.find(params[:id])
+        
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
     
     def edit
-        @user = User.find(params[:id])
+        
     end
 
     def update 
-        @user = User.find(params[:id])
+        
         if @user.update(user_params)
             flash[:notice] = "Your account information was successfully updated"
             redirect_to @user #this is short for user path, which require an user obj
@@ -32,6 +32,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
+            session[:user_id] = @user.id
             redirect_to articles_path
         else
             render 'new'
@@ -41,5 +42,9 @@ class UsersController < ApplicationController
     private 
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
     end
 end
