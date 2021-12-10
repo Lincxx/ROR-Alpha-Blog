c9e9f45af79b8910fdb8342bb96930a991c2a232
Jeremy Lincoln <jeremy.lincoln@bisonline.com>
2021-12-10 09:21:15 -0500

    Add display of number of articles associated with each category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,6 +13,7 @@ class CategoriesController < ApplicationController
     def show
         # byebug
         @category = Category.find(params[:id])
+        @articles = @category.articles.paginate(page: params[:page], per_page: 5)
     end
 
     def create
